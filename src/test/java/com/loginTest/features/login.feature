f1dee25ac682d1a27d6457e4b4a7d58780174210
Durvesh <durveshpokade835@gmail.com>
2024-09-19 18:55:51 +0530

    added regular Expressions

--- src/test/java/com/loginTest/features/login.feature
+++ src/test/java/com/loginTest/features/login.feature
@@ -1,16 +1,34 @@
+#Feature: Login Functionality
+#
+#  Scenario: Login with valid credentials and verify successful login
+#    Given I am on the login page
+#    When User login with "hardstop-pharmacist6@mailinator.com" and "Password1#"
+#    Then I should be redirected to the dashboard
+#
+#  Scenario: Login with invalid password and verify error message
+#    Given I am on the login page
+#    When User login with "hardstop-pharmacist6@mailinator.com" and "Password1#"
+#    Then I should see an error message "Wrong username/password"
+#
+#  Scenario: Login with invalid email and verify error message
+#    Given I am on the login page
+#    When User login with "hardstop-pharmacist6@mailinator.com" and "Password1#"
+#    Then I should see an error message "User not found"
+
+
 Feature: Login Functionality
 
   Scenario: Login with valid credentials and verify successful login
-    Given I am on the login page
-    When I enter valid credentials
-    Then I should be redirected to the dashboard
+    Given User on the login page
+    When User login with "hardstop-pharmacist6@mailinator.com" and "Password1#"
+    Then User should be redirected to the dashboard
 
   Scenario: Login with invalid password and verify error message
-    Given I am on the login page
-    When I enter an invalid password
-    Then I should see an error message "Wrong username/password"
+    Given User on the login page
+    When User login with "hardstop-pharmacist6@mailinator.com" and "Password"
+    Then User should see an error message "Wrong username/password"
 
   Scenario: Login with invalid email and verify error message
-    Given I am on the login page
-    When I enter an invalid email
-    Then I should see an error message "User not found"
+    Given User on the login page
+    When User login with "hardstop-pharmacist6@mailinator.co" and "Password1#"
+    Then User should see an error message "User not found"
--- src/test/java/com/loginTest/stepDefinitions/LoginSteps.java
+++ src/test/java/com/loginTest/stepDefinitions/LoginSteps.java
@@ -1,3 +1,57 @@
+//package com.loginTest.stepDefinitions;
+//
+//import com.loginTest.pages.PharmaLogic;
+//import com.loginTest.utils.DriverUtils;
+//import io.cucumber.java.en.Given;
+//import io.cucumber.java.en.When;
+//import io.cucumber.java.en.Then;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.testng.Assert;
+//
+//public class LoginSteps extends DriverUtils {
+//    WebDriver driver = new ChromeDriver();
+//    PharmaLogic pharmaLogic = new PharmaLogic(driver);
+//
+//    @Given("I am on the login page")
+//    public void i_am_on_the_login_page() {
+//        pharmaLogic.openLoginPage();
+//    }
+//
+//    @When("I enter valid credentials")
+//    public void i_enter_valid_credentials() {
+//        pharmaLogic.enterEmail("hardstop-pharmacist6@mailinator.com");
+//        pharmaLogic.enterPassword("Password1#");
+//        pharmaLogic.clickSignIn();
+//    }
+//
+//    @When("I enter an invalid password")
+//    public void i_enter_an_invalid_password() {
+//        pharmaLogic.enterEmail("hardstop-pharmacist6@mailinator.com");
+//        pharmaLogic.enterPassword("Password");
+//        pharmaLogic.clickSignIn();
+//    }
+//
+//    @When("I enter an invalid email")
+//    public void i_enter_an_invalid_email() {
+//        pharmaLogic.enterEmail("hardstop-pharmacist6@mailinator.co");
+//        pharmaLogic.enterPassword("Password1#");
+//        pharmaLogic.clickSignIn();
+//    }
+//
+//    @Then("I should be redirected to the dashboard")
+//    public void i_should_be_redirected_to_the_dashboard() {
+//        Assert.assertTrue(pharmaLogic.isDashboardDisplayed(), "Dashboard is not displayed");
+//    }
+//
+//    @Then("I should see an error message {string}")
+//    public void i_should_see_an_error_message(String expectedMessage) {
+//        String actualMessage = pharmaLogic.getErrorMessage();
+//        Assert.assertEquals(actualMessage, expectedMessage, "Error message does not match");
+//    }
+//}
+
+
 package com.loginTest.stepDefinitions;
 
 import com.loginTest.pages.PharmaLogic;
@@ -13,39 +67,25 @@ public class LoginSteps extends DriverUtils {
     WebDriver driver = new ChromeDriver();
     PharmaLogic pharmaLogic = new PharmaLogic(driver);
 
-    @Given("I am on the login page")
-    public void i_am_on_the_login_page() {
+    @Given("^User on the login page$")
+    public void user_on_the_login_page() {
         pharmaLogic.openLoginPage();
     }
 
-    @When("I enter valid credentials")
-    public void i_enter_valid_credentials() {
-        pharmaLogic.enterEmail("hardstop-pharmacist6@mailinator.com");
-        pharmaLogic.enterPassword("Password1#");
+    @When("^User login with \"([^\"]*)\" and \"([^\"]*)\"$")
+    public void user_login_with_credentials(String email, String password) {
+        pharmaLogic.enterEmail(email);
+        pharmaLogic.enterPassword(password);
         pharmaLogic.clickSignIn();
     }
 
-    @When("I enter an invalid password")
-    public void i_enter_an_invalid_password() {
-        pharmaLogic.enterEmail("hardstop-pharmacist6@mailinator.com");
-        pharmaLogic.enterPassword("Password");
-        pharmaLogic.clickSignIn();
-    }
-
-    @When("I enter an invalid email")
-    public void i_enter_an_invalid_email() {
-        pharmaLogic.enterEmail("hardstop-pharmacist6@mailinator.co");
-        pharmaLogic.enterPassword("Password1#");
-        pharmaLogic.clickSignIn();
-    }
-
-    @Then("I should be redirected to the dashboard")
-    public void i_should_be_redirected_to_the_dashboard() {
+    @Then("^User should be redirected to the dashboard$")
+    public void user_should_be_redirected_to_the_dashboard() {
         Assert.assertTrue(pharmaLogic.isDashboardDisplayed(), "Dashboard is not displayed");
     }
 
-    @Then("I should see an error message {string}")
-    public void i_should_see_an_error_message(String expectedMessage) {
+    @Then("^User should see an error message \"([^\"]*)\"$")
+    public void user_should_see_an_error_message(String expectedMessage) {
         String actualMessage = pharmaLogic.getErrorMessage();
         Assert.assertEquals(actualMessage, expectedMessage, "Error message does not match");
     }
